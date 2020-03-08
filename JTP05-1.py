89614ae493d0ffbd9fa13b17c5088b5a0f2f3f06
LJLee37 <oh-my-zsh@kakao.com>
2020-03-08 21:29:04 +0900

    8th Mar learning

---
+++ JTP05-1.py
@@ -0,0 +1,15 @@
+class FourCal:
+    def __init__(self):
+        self.a = 0
+        self.b = 0
+    def setNum(self, a, b):
+        self.a = a
+        self.b = b
+    def add(self):
+        return self.a + self.b
+    def sub(self):
+        return self.a - self.b
+    def mul(self):
+        return self.a * self.b
+    def div(self):
+        return self.a / self.b
